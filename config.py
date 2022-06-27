eb2c7f9ad1877164ce931d85ac431edb7f57f2f7
jung hyun <hfghhghg11@gmil.com>
2022-06-27 18:07:52 +0900

    app

---
+++ config.py
@@ -0,0 +1,18 @@
+class Config :
+    JWT_SECRET_KEY = 'yhacademy1029##hello'
+    JWT_ACCESS_TOKEN_EXPIRES = False # 로그아웃 시간
+    PROPAGATE_EXCEPTIONS = True
+
+    # AWS PK
+    ACCESS_KEY = 'AKIAZPUXAZQKKMXICDCL'
+    SECRET_ACCESS = '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'
+
+
+    # S3 버킷이름과 기본 url 주소 셋팅
+    S3_BUCKET = 'pk-image-test'
+    S3_LOCATION = 'https://pk-image-test.s3.amazonaws.com/'
+
+    NAVER_PAPAGO_URL = 'https://openapi.naver.com/v1/papago/n2mt'
+
+    NAVER_CLIENT_ID = 'Djidio5sW87iKVCzshZE'
+    NAVER_CLIENT_SECRET = 'qYBBvvXBZO'
